921856a752f747d6ca75fb492ec720a650e10f3a
Gianfranco-98 <gianfrancodimarco98@outlook.it>
2023-10-24 17:20:09 +0200

    Debugging

--- api/update_fun.js
+++ api/update_fun.js
@@ -4,7 +4,7 @@ console.log("ALJKDLAJKJSLDKAJLAKSJDLKJADLZIJOL|IJL")
 module.exports = (req, res) => {
     // Verify the request method is POST
     if (req.method === "POST") {
-        console.log("skdsjhfksjhksjhkjsdhfksd")
+        console.error("skdsjhfksjhksjhkjsdhfksd")
         try {
             // Parse the request body as JSON
             console.log("BEFORE DATA")
