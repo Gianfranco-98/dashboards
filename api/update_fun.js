63ec3294fde9a82551d8bd5d3e3e807d71a8e217
Gianfranco-98 <gianfrancodimarco98@outlook.it>
2023-10-24 17:53:36 +0200

    Solved parsing errors

--- api/update_fun.js
+++ api/update_fun.js
@@ -1,39 +1,18 @@
-let numberOfPeople = 0; // Initialize with an initial value
-console.log("ALJKDLAJKJSLDKAJLAKSJDLKJADLZIJOL|IJL")
+let numberOfPeople = 0;
 
 module.exports = async (req, res) => {
     // Verify the request method is POST
     if (req.method === "POST") {
-        const data = JSON.parse(req.body);
-        //console.log("DATA")
-        //console.log(data)
-        numberOfPeople = data.numberOfPeople;
-        //console.log(numberOfPeople)
-
-        // Respond with a success message
-        res.status(200).json({ message: "Data updated successfully" });
-        /*try {
-            // Parse the request body as JSON
-            res.status(100).json({ error: "HEYLA" })
+        try {
             const data = JSON.parse(req.body);
-            console.log("DATA")
-            console.log(data)
             numberOfPeople = data.numberOfPeople;
-            //console.log(numberOfPeople)
-
-            // Respond with a success message
             res.status(200).json({ message: "Data updated successfully" });
-
         } catch (error) {
-            // Handle any errors
             res.status(500).json({ error: "Internal server error" });
-        }*/
+        }
     } else if (req.method === "GET") {
-        // Handle GET request to retrieve the number of people waiting
-        console.log("A<SJDHAKLSJHDLKAJ");
         res.status(200).json({ numberOfPeople });
     } else {
-        // Return an error for unsupported request methods
         res.status(405).json({ error: "Method not allowed" });
     }
 };
