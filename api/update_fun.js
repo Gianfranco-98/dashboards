bd43c7083a5f92ffec042dca5b2c664d49701226
Gianfranco-98 <gianfrancodimarco98@outlook.it>
2023-10-24 16:19:27 +0200

    Debugging

--- api/update_fun.js
+++ api/update_fun.js
@@ -1,18 +1,24 @@
+let numberOfPeople = 0; // Initialize with an initial value
+
 module.exports = (req, res) => {
     // Verify the request method is POST
     if (req.method === "POST") {
         try {
             // Parse the request body as JSON
-            const numberOfPeople = JSON.parse(req.body);
-            console.log(numberOfPeople)
+            const data = JSON.parse(req.body);
+            numberOfPeople = data.numberOfPeople;
+            //console.log(numberOfPeople)
 
             // Respond with a success message
-            res.status(200).json({ numberOfPeople });
+            res.status(200).json({ message: "Data updated successfully" });
 
         } catch (error) {
             // Handle any errors
             res.status(500).json({ error: "Internal server error" });
         }
+    } else if (req.method === "GET") {
+        // Handle GET request to retrieve the number of people waiting
+        res.status(200).json({ numberOfPeople });
     } else {
         // Return an error for unsupported request methods
         res.status(405).json({ error: "Method not allowed" });
