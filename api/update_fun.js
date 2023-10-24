5df42f4b4f493a5f9124e98c3636c5d13cd49ace
Gianfranco-98 <gianfrancodimarco98@outlook.it>
2023-10-24 15:18:14 +0200

    Added REST API draft files

---
+++ api/update_fun.js
@@ -0,0 +1,19 @@
+module.exports = (req, res) => {
+    // Verify the request method is POST
+    if (req.method === "POST") {
+        try {
+            // Parse the request body as JSON
+            const numberOfPeople = JSON.parse(req.body);
+
+            // Respond with a success message
+            res.status(200).json({ numberOfPeople });
+
+        } catch (error) {
+            // Handle any errors
+            res.status(500).json({ error: "Internal server error" });
+        }
+    } else {
+        // Return an error for unsupported request methods
+        res.status(405).json({ error: "Method not allowed" });
+    }
+};
