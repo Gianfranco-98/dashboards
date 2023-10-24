12fa454864559ad09f6e320d57f59e55e497b03d
Gianfranco-98 <gianfrancodimarco98@outlook.it>
2023-10-24 17:29:31 +0200

    Debugging

--- api/update_fun.js
+++ api/update_fun.js
@@ -4,22 +4,30 @@ console.log("ALJKDLAJKJSLDKAJLAKSJDLKJADLZIJOL|IJL")
 module.exports = (req, res) => {
     // Verify the request method is POST
     if (req.method === "POST") {
-        try {
+        const data = JSON.parse(req.body);
+        //console.log("DATA")
+        //console.log(data)
+        numberOfPeople = data.numberOfPeople;
+        //console.log(numberOfPeople)
+
+        // Respond with a success message
+        res.status(200).json({ message: "Data updated successfully" });
+        /*try {
             // Parse the request body as JSON
             res.status(100).json({ error: "HEYLA" })
-            /*const data = JSON.parse(req.body);
+            const data = JSON.parse(req.body);
             console.log("DATA")
             console.log(data)
             numberOfPeople = data.numberOfPeople;
             //console.log(numberOfPeople)
 
             // Respond with a success message
-            res.status(200).json({ message: "Data updated successfully" });*/
+            res.status(200).json({ message: "Data updated successfully" });
 
         } catch (error) {
             // Handle any errors
             res.status(500).json({ error: "Internal server error" });
-        }
+        }*/
     } else if (req.method === "GET") {
         // Handle GET request to retrieve the number of people waiting
         console.log("A<SJDHAKLSJHDLKAJ");
