0b30d1a310c716bcdf9be8f46e97fb55b847355a
Gianfranco-98 <gianfrancodimarco98@outlook.it>
2023-10-26 10:42:16 +0200

    Bug fixes

--- api/update_fun.js
+++ api/update_fun.js
@@ -7,6 +7,7 @@ let reset = false;
 
 // API endpoint
 module.exports = (req, res) => {
+    res.setHeader('Cache-Control', 'no-store, max-age=0');
     // -> Handling POST requests
     if (req.method === "POST") {
         try {
@@ -43,11 +44,6 @@ module.exports = (req, res) => {
         }
     // -> Handling GET requests
     } else if (req.method === "GET") {
-        if (reset) {
-            numberOfPeople = 0;
-            totalWaited = 0;
-            avgWaitingTime = 0;
-        }
         res.status(200).json({ numberOfPeople, totalWaited, avgWaitingTime });
     } else {
         res.status(405).json({ error: "Method not allowed" });
